80284524c1f6b591b689cee0cff47a6d7924298e
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-04-19 16:43:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
+//     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+//     .then(res => res.json())
+//     .then(name => addNamesToDOM(name))
+// })
+const getData = () => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
     .then(res => res.json())
-    .then(pokeNames => addNamesToDOM(pokeNames))
-})
+    .then(name => displayName(name))
+}
 
+// const span = document.createElement('span')
+//         span.innertext=charObj.name
+//         span.addEventListener('click', handleClick)
+//         document.getElementById('charater-bar').append(span)
+
+const displayName = (arr) => {
+    arr.forEach(pokeObj)= () => {
+        const span = document.createElement('span')
+        span.innertext=pokeObj.name
+        span.addEventListener('click', handleClick)
+        document.getElementById('pokeName').append(span)
+    }
+}
