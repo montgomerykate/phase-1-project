5f5d877ef10217ac746a4649805f72c9dd0c4e00
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-04-19 15:57:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    .then(res => res.json())
+    .then(pokeNames => addNamesToDOM(pokeNames))
+})
 
