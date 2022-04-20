63b7c1d4903efaa545d0645764d944677a6d2bc5
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-04-20 16:56:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ document.addEventListener('DOMContentLoaded', () => {
           const img =document.createElement("img")
           img.src = pokeNames.image
           img.id = pokeNames.id
+          document.getElementById('pokeimages').append(img)
       })
   }
 
+ const pokemonName =() => {
+     image.addEventListener("mouseover", () => {
+         
+     })
+ }
+
+  const styleElements = (frame, image) => {
+    frame.style.display = 'inline-block'
+    frame.style.flexDirection = 'row';
+    image.style.width = '350px';
+    image.style.height = '350px';
+    image.style.borderRadius = '1.5%';
+    image.style.marginLeft = '2%';
+    image.style.marginRight = '2%';
+    image.style.marginTop = '2%';
+}
+
+  const eventListeners = (image, pokemon) => {
+    image.addEventListener('mouseenter', () => {
+        image.style.height = '500px';
+        image.style.width = '500px';
+        image.style.left = timePassed / 5 + 'px';
+    })
+    image.addEventListener('mouseleave', () => {
+        image.style.height = '350px';
+        image.style.width = '350px';
+    })
+    const frame = document.createElement('div');
+}
