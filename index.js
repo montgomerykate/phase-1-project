f8b74d53729f360c5cede8fa70c7aa32887cf508
montgomerykate <101065319+montgomerykate@users.noreply.github.com>
2022-04-20 15:22:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/pokeNames')
+      .then(resp => resp.json())
+      .then(pokemon => renderAllPokemon(pokemon))
+  });
+
+  const renderAllPokemon = (arrOfObj) => {
+      const div = document.getElementById('detailed-info')
+      arrOfObj.forEach(pokeNames => {
+          const img =document.createElement("img")
+          img.src = pokeNames.image
+          img.id = pokeNames.id
+      })
+  }
 
